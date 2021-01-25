496b384417506a405a54a779934af1b9304c8cf1
zhaori <1471584500@qq.com>
2021-01-25 17:29:11 +0800

    重写增量备份功能

--- work/incremental_backup.py
+++ work/incremental_backup.py
@@ -1,152 +1,148 @@
-# 增量备份
 """
-只备份不同的数据，但当天的总备份量为 完全备份+以往每天的增量备份总数据
+Python version:3.7 x64
+OS: Windows10 64
+Pycharm version: 3.2
+Github Index: https://github.com/zhaori
+增量备份：设：y为当天备份总数据 z为当前新的数据，a为完整备份文件，x为当天。用！=表示两者差异部分的数据，表达式为下
+y = a+(z!=(x-1))
+解释：除了首次备份，所有的备份都是用新数据与上次备份的数据进行比较，然后每次备份的数据只是当期比较的差异数据
+例如：我有个文件夹存放的有a b c 三个元素，然后
+第一天新增了 d 那么第一次开始增量备份 是 abc: d
+第二天新增了e 那么第二次的增量备份是 abc: e
+第三天删除了a,那么第三次增量备份是 abc: bc
 """
-
 import os
 import shutil
 from tkinter.messagebox import showinfo
 
-from Lib.Pydos import sys_copy
+from Lib.Pydos import sys_copy, del_folder, del_file
 from Lib.safety.Hash import Hash
+from Lib.sqlite import Create_db, list_to_str
 from Lib.z7 import archive
-from config.Differential_Config import time_folder
-from config.Main_Config import READ_DB
+from config.DB_Config import db_table, db_mode, db_data
+from config.Main_Config import BACKUP_PATH, READ_DB, TEMP_PATH
+from work.gettime import now_time
+
+
+def original_file():
+    # 用一份完全备份包作为底包，意味着一切备份的依据都是这个
+    basename_folder = os.path.basename(READ_DB)  # 不包含文件夹路径，只取文件夹名字
+    copy_full = os.path.join(fr'.\backups\incremental\{basename_folder}', f"{basename_folder}.7z")
+    full_backup = os.path.abspath(os.path.join(BACKUP_PATH, f"{basename_folder}.7z"))  # 完全备份压缩包的相对路径
+    full_backup_db = os.path.abspath(os.path.join(BACKUP_PATH, f"{basename_folder}.db"))  # 完全备份压缩包的相对路径
+    copy_full_db = os.path.join(fr'.\backups\incremental\{basename_folder}', f"{basename_folder}.db")
+    return [copy_full, full_backup, full_backup_db, copy_full_db]
+
+
+def copy_ready():
+    # 把完全备份文件拷贝到增量备份文件夹里作为备份的依据
+    sys_copy(original_file()[1], original_file()[0])
+    sys_copy(original_file()[2], original_file()[3])
+
+
+def unzip_backup():
+    # 将底包解压到临时文件夹总，然后与现存文件夹的内容进行对比
+    archive.unzip(original_file()[0], TEMP_PATH)
+
+
+def find_child_folder(path: str, data: str):
+    """
+    :param path: 绝对路径
+    :param data: 查询数据
+    :return: 返回指定文件夹的子目录相对路径
+    """
+    p = path[path.find(data):]
+    return p[len(data) + 1: len(p)]
+
+
+old_file_list = []  # 原始完整备份的文件
+old_folder_list = []  # 原始完整备份的文件夹
+old_dict = dict()  # 旧文件哈希值字典
+new_file_list = []  # 最新文件
+new_folder_list = []  # 最新文件夹
+new_dict = dict()  # 新文件哈希值字典
+add_backup = set()  # 添加入备份的集合，用现存数据与原始备份数据对比得到的
+del_backup = set()  # 删除文件/文件夹的集合，是用原始备份数据对比现存数据得到的
 
 
 def incremental_backup():
-    basename_folder = os.path.basename(READ_DB)  # 不包含文件夹路径，只取文件夹名字
-    temp_folder = r".\Temp"  # 临时文件夹
+    if original_file()[0] not in os.listdir(fr'.\backups\incremental\{os.path.basename(READ_DB)}'):
+        # 如果完全备份文件不存在就拷贝过来
+        copy_ready()
 
-    new_backup = r'.\backups\incremental\{}\{}'.format(basename_folder, time_folder)  # 生成以日期命名的文件夹
+    unzip_backup()
+    time = str(type('now', (), {'__repr__': lambda s: now_time()})())
+    basename_folder = os.path.basename(READ_DB)  # 不包含文件夹路径，只取文件夹名字
+    new_backup = r'.\backups\incremental\{}\{}'.format(basename_folder, time)  # 生成以日期命名的文件夹
     if os.path.exists(new_backup) is False:
         os.makedirs(new_backup)
 
-    if not os.listdir(new_backup):  # 以最新的一次差异备份的备份文件做备份底包
-        basename_folder = os.path.basename(READ_DB)  # 不包含文件夹路径，只取文件夹名字
-        diff_backup = r'.\backups\TimeBackup\{}'.format(basename_folder)
-        copy_file = os.listdir(diff_backup)[-1]
-        filepath, file = os.path.split(copy_file)
-        sys_copy(os.path.join(diff_backup, copy_file), r'.\backups\incremental\{}\{}'.format(basename_folder, file))
+    read_full_db = Create_db(db_table, db_mode, db_data, original_file()[3])
+    old_file_hash = [list_to_str(i) for i in read_full_db.search_sql('file_hash')]  # 原始完整备份的文件哈希值
 
-    # 将上一次备份的文件解压到临时文件夹中
-    file_zip_path = r'.\backups\incremental\{}'.format(basename_folder)
-    file_list = None
-    root_folder = None
-    for root, dirs, file in os.walk(file_zip_path):
-        if root and file:
-            file_list = file
-            root_folder = root
-    try:
-        file_zip_file = file_list[-1]
-    except TypeError:
-        raise BaseException('无其他增量备份文件, 先运行 “第一次增量备份先运行此文件.py”')
+    for root, dirs, filename in os.walk(TEMP_PATH):
+        for file in filename:
+            old_file_list.append(os.path.join(root, file))
+        for dir_name in dirs:
+            old_folder_list.append(os.path.join(root, dir_name))
 
-    unzip = os.path.join(root_folder, file_zip_file)
-    archive.unzip(unzip, temp_folder)
+    # 这里，因为需要与现在的文件夹里的内容做对比，但是因为将完全备份的文件解压到临时文件的原因，导致路径不同，会被视为两个不同的文件所以无法准确地对比
+    # 因此，多做一步，将临时文件夹路径改成源文件的备份路径
+    old_file = [file.replace(r'.\Temp', READ_DB) for file in old_file_list]
+    old_folder = [folder.replace(r'.\Temp', READ_DB) for folder in old_folder_list]
 
-    old_file_list = []  # 完整备份原始文件
-    old_path_list = []  # 完整备份原始文件夹
-    old_file_hash_list = []  # 完整备份的文件哈希值列表
-    for temp_root, temp_dirs, temp_file in os.walk(temp_folder):
-        for name in temp_file:
-            old_file_list.append(os.path.join(temp_root, name))
-            old_file_hash_list.append(Hash(os.path.join(temp_root, name)).md5())
+    for root, dirs, filename in os.walk(READ_DB):
+        for file in filename:
+            new_file_list.append(os.path.join(root, file))
+        for dir_name in dirs:
+            new_folder_list.append(os.path.join(root, dir_name))
 
-    now_file_list = []  # 现在的文件
-    now_folder_list = []  # 现在的文件夹
-    for root, basename, filename in os.walk(READ_DB):
-        for f in filename:
-            now_file = os.path.join(root, f).replace('/', '\\')
-            now_file_list.append(now_file)
-        now_folder_list.append(root.replace('/', '\\'))
+    for _old_file, _old_hash in zip(old_file_list, old_file_hash):
+        old_dict[_old_file] = _old_hash
 
-    # 通过验证哈希值的不同来判断文件是否已经修改
-    # 验证文件哈希值，如果哈希值相同但名称不同，证明文件名被修改
-    # 如果老文件与新文件都存在，但新文件哈希值不等于老文件哈希值，证明文件被修改过
+    for file in new_file_list:
+        if file in old_file and Hash(file).md5() not in old_file_hash:
+            # 判断文件是否备份修改，如果文件名相同，但哈希值不同可以判定为被修改
+            add_backup.add(file)
 
-    def hash_or_file(data):
-        hash_file_dict = dict()  # 通过哈希值找寻源文件名
-        for key, value in zip(old_file_hash_list, old_file_list):
-            if data == 'hash':  # 通过哈希值返回文件
-                hash_file_dict[key] = value
-            elif data == 'file':
-                hash_file_dict[value] = key
-        return hash_file_dict
+        elif file not in old_file and Hash(file).md5() not in old_file_hash:
+            # 如果文件不在备份文件里，而且哈希值也不在旧备份文件的哈希值里，可以判断是新增的文件
+            # 记录一下问题，如果说原备份文件里存在空白文件，然后新增文件中也存在空白文件，就无法判定文件存在
+            add_backup.add(file)
 
-    add_data = []  # 提交到数据库的数据
-    del_data = []  # 需要删除的数据
-    for new_file in now_file_list:
-        # 新文件不存在旧文件列表中且新文件的哈希值在旧文件哈希值列表中时，判定文件被重命名
-        if new_file not in old_file_list and Hash(new_file).md5() in old_file_hash_list:
-            _diff_data = {
-                "mode": "rename",
-                "difference": new_file,
-                "source": hash_or_file('hash')[Hash(new_file).md5()],
-                "reduce": "{}.7z".format(new_backup),
-                "now_time": time_folder
-            }
-            add_data.append(new_file)
+        elif file not in old_file and Hash(file).md5() in old_file_hash:
+            # 如果文件不在备份文件里，但哈希值存在于旧备份文件，可以判定是被重命名
+            add_backup.add(file)
 
-        # 新文件在旧文件列表中且新文件哈希值不存在旧文件哈希值列表中，判定文件被修改
-        elif new_file in old_file_list and Hash(new_file).md5() not in old_file_hash_list:
-            add_data.append(new_file)
+    for folder in new_folder_list:
+        if folder not in old_folder:
+            # 如果文件夹不在备份文件夹，在新数据里，表示文件夹是新增文件夹
+            add_backup.add(folder)
 
-        # 新文件不在旧文件列表中且哈希值在旧文件哈希值中，判定文件夹被重命名或被移动
-        elif new_file not in old_file_list and Hash(new_file).md5() in old_file_hash_list:
-            add_data.append(new_file)
+    for file in old_file:
+        if file not in new_file_list:
+            # 如果新文件不在备份文件里，则判定被删除
+            del_backup.add(file)
 
-        # 新文件不存在旧文件列表中，判定文件是新增
-        elif new_file not in old_file_list:
-            add_data.append(new_file)
+    for folder in old_folder:
+        if folder not in new_folder_list:
+            # 如果新文件夹不在备份数据里，则判定文件夹被删除
+            del_backup.add(folder)
 
-    now_file_hash = [Hash(i).md5() for i in now_file_list]
-    for old_file in old_file_list:
-        if old_file not in now_file_list and Hash(old_file).md5() not in now_file_hash:
-            # 如果旧文件不在新文件列表中，判断是被删除
-            add_data.append(old_file)
-            del_data.append(old_file)
+    for data in add_backup:
+        if os.path.isdir(data):
+            shutil.copytree(data, os.path.join(TEMP_PATH, os.path.basename(data)))
+        elif os.path.isfile(data):
+            shutil.copy(data, os.path.join(TEMP_PATH, os.path.basename(data)))
 
-    for _old in old_path_list:
-        if _old not in now_folder_list:
-            # 当老文件夹不在现在的文件夹，删除
-            add_data.append(_old)
-            del_data.append(_old)
+    for data in del_backup:
+        # 这里，因为系统是无法判断，是空文件夹还是文件，比如 D:\text 可能是空文件夹或者是一个没有后缀的文件，因此
+        # 第一步是用删除文件夹的方法强制删除，第二步是删除文件
+        del_folder(os.path.join(TEMP_PATH, find_child_folder(data, READ_DB)))
+        del_file(os.path.join(TEMP_PATH, find_child_folder(data, READ_DB)))
 
-    for _new in now_folder_list:
-        # 这里是新文件不在老文件夹中的时候
-        if _new not in old_path_list:
-            add_data.append(_new)
-
-    def find_child_folder(path, data):
-        """
-         #:param path: 绝对路径
-         #:param data: 查询数据
-         #:return: 返回指定文件夹的子目录相对路径
-        """
-        p = path[path.find(data):]
-        return p[len(data) + 1: len(p)]
-
-    for info in add_data:
-        temp_file = os.path.join(temp_folder, find_child_folder(info, basename_folder))
-        filepath, file = os.path.split(temp_file)
-        if os.path.exists(filepath) is False:
-            os.makedirs(filepath)
-        if info not in os.listdir(temp_folder):  # 避免自己复制自己
-            if os.path.isfile(info):
-                sys_copy(info, temp_file)
-            elif os.path.isdir(info):
-                try:
-                    shutil.copytree(info, temp_file)
-                except FileExistsError:
-                    pass
-
-    #  解压完全备份文件,与增量备份合并
-    # new_zip = archive(data_path, READ_DB)
-    # file_7z = '{}/{}.7z'.format(data_path, basename_folder)
-    # new_zip.unzip(file_7z, temp_folder)  # 解压到TEMP临时文件夹
-    os.system(r'{} -mx5 -t7z a {} {}\* -mmt -sdel'.format('7z', '{}'.format(new_backup), temp_folder))
-    os.system('rd {}'.format(new_backup))
+    os.system(r'{} -mx5 -t7z a {} {}\* -mmt -sdel'.format('7z', '{}'.format(new_backup), TEMP_PATH))
+    os.system(f'rd {new_backup}')
     showinfo('提示', '备份成功')
 
 
